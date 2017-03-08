3f5ec892ec6b136acc4ee9b8163a8515747e0391
PrakharGarg <prakhargarg@utexas.edu>
2017-03-08 14:06:31 -0600

    routes. Issue #7

--- app/views.py
+++ app/views.py
@@ -2,7 +2,40 @@ from flask import render_template
 from app import app
 
 # Index page that contains a carousel 
+# Can be accessed at either / or /index/
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/index/')
+def index() :
     return render_template('index.html')
+
+# Page that shows all of the beers in a grid    
+@app.route('/beers/')
+def beers() :
+    return render_template('beers.html')
+
+# Page that shows info about a specific beer with
+# the name of the beer being passed in   
+@app.route('/beers/<beer_name>/')
+def beer(beer_name) :
+    return render_template('beer.html', name = beer_name)
+
+# Page that shows all of the venues in a grid
+@app.route('/venues/')
+
+# Page that shows info about a specific venue with
+# the name of the venue being passed in 
+@app.route('/venues/<venue_name>/')
+
+# Page that shows all of the breweries in a grid
+@app.route('/breweries/')
+
+# Page that shows info about a specific brewery with
+# the name of the brewery being passed in 
+@app.route('/breweries/<brewery_name>/')
+
+# Page that shows all of the locations in a grid
+@app.route('/locations/')
+
+# Page that shows info about a specific location with
+# the name of the location being passed in 
+@app.route('/locations/<location_name>/')
