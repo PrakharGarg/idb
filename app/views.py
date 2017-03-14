a8bdae80ca02a5c9a3dd568ac0ba74f62d598460
PrakharGarg <prakhargarg@utexas.edu>
2017-03-14 12:37:11 -0500

    added all the views. Issue #16

--- app/views.py
+++ app/views.py
@@ -21,21 +21,32 @@ def beer(beer_name) :
 
 # Page that shows all of the venues in a grid
 @app.route('/venues/')
+def venues() :
+    return render_template('venues.html')
 
 # Page that shows info about a specific venue with
 # the name of the venue being passed in 
 @app.route('/venues/<venue_name>/')
-
+def venue(venue_name) :
+    return render_template('venue.html', name = venue_name)
 # Page that shows all of the breweries in a grid
 @app.route('/breweries/')
-
+def breweries() :
+    return render_template('breweries.html')
+    
 # Page that shows info about a specific brewery with
 # the name of the brewery being passed in 
 @app.route('/breweries/<brewery_name>/')
+def brewery(brewery_name) :
+    return render_template('brewery.html', name = brewery_name)
 
 # Page that shows all of the locations in a grid
 @app.route('/locations/')
+def locations() :
+    return render_template('locations.html')
 
 # Page that shows info about a specific location with
 # the name of the location being passed in 
 @app.route('/locations/<location_name>/')
+def location(location_name) :
+    return render_template('location.html', name = location_name)
