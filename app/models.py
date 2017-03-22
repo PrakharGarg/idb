fe05fff4f24ed6749f8fa062d5cc6ee19763a217
Patrick Moore <phatrickmoore@gmail.com>
2017-03-22 14:20:25 -0500

    added good comments outlining the attributes, their purposes

--- app/models.py
+++ app/models.py
@@ -17,6 +17,18 @@ venue_beer_association = db.Table('venue_beer_association',
 model definitions
 """
 class Beer(db.Model):
+	"""
+	This is the beer model. It contains the necessary information and links
+	for serving up all you could ever want to know about a tasty brew.
+
+	Attributes:
+		name: name of the beer
+		labels: link to a picture of the beer label
+		style: what type of beer this is (ale, lager, IPA, kolsh, etc.)
+		is_organic: whether or not this beer is environmentally friendly!
+		brewery: where and who this is brewed by 
+		venues: where this beer is served, sold, and distributed
+	"""
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120), index=True, unique=True)
 	labels = db.Column(db.Array(db.String(300)), index=True, unique=True)
@@ -26,6 +38,19 @@ class Beer(db.Model):
 	venues = db.relationship('Venue', secondary=venue_beer_association)
 
 class Brewery(db.Model):
+	"""
+	The brewery model. This gives information about what kinds of beers
+	are crafted here, what kind of brewery tis is, and where it's located.
+
+	Attributes:
+		name: name of this brewery
+		brewery_type: large, medium, craft, micro, etc.
+		founded: date this brewery was created
+		labels: links to images of this breweries logo
+		address: where this thing is!
+		venues: who serves, sells, and distributes this beer
+		state: which state this exists in
+	"""
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120), index=True, unique=True)
 	brewery_type = db.Column(db.String(60), index=True, unique=True)
@@ -36,6 +61,22 @@ class Brewery(db.Model):
 	state = db.relationship('State')
 
 class Venue(db.Model):
+	"""
+	The venue model. You come here to visit with friends, 
+	enjoy a pint of your favorite brew, and to find 
+	new favorites! This has all the info you need. For
+	bars, restaurants, stores, and anywhere else you can get
+	your hands on a can, bottle, or pint.
+
+	Attributes:
+		name: its name
+		media: links to images of the venue, its patrons, its logo, etc.
+		address: where this is located
+		category: is this a bar? is it a store? this will tell you!
+		state: which state you will find this in
+		breweries: all of the breweries represented here
+		beers: all the beers you may obtain
+	"""
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120), index=True, unique=True)
 	media = db.Column(db.Array(db.String(300)), index=True, unique=True)
@@ -46,6 +87,16 @@ class Venue(db.Model):
 	beers = db.relationship('Beer', secondary=venue_beer_association)
 
 class State(db.Model):
+	"""
+	A large geographic region you may wish to explore by beer. Beer!
+	States contain information about venues and breweries located in them,
+	and some general trivia you may find amusing.
+
+	Attributes:
+		name: what state is this?
+		capital: the city where this state does its legislative business
+		media: links to pics of this state! 
+	"""
 	id = db.Column(db.Integer, primary_key=True)
 	capital = db.Column(db.String(120), index=True, unique=True)
 	name = db.Column(db.String(120), index=True, unique=True)
