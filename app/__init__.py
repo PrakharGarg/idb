d51ec6edee2c1e20b700a506326eaf9c2751cf83
PrakharGarg <prakhargarg@utexas.edu>
2017-03-23 08:36:00 -0500

    comment out models for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+# app.config.from_object('config')
+# db = SQLAlchemy(app)
 
-from app import views, models
+from app import views
