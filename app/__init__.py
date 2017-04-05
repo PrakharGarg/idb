940b76c491298fdb997bdc776fa6bb4f2a329bb7
PrakharGarg <prakhargarg@utexas.edu>
2017-04-05 17:44:02 -0500

    meh

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.triangle import Triangle
+
 
 app = Flask(__name__)
+Triangle(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/poh'
 db = SQLAlchemy(app)
 
