bf79a67dc1d19782570f60200d630e1e2effcb7d
PrakharGarg <prakhargarg@utexas.edu>
2017-04-05 22:00:14 -0500

    local db

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import sensitive
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = sensitive.DATABASE['postgres']
+# app.config['SQLALCHEMY_DATABASE_URI'] = sensitive.DATABASE['postgres']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/poh'
 db = SQLAlchemy(app)
 db.create_all()
 
