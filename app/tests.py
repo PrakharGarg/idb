3b997701698e3199647a7069d66196f9e00accbe
Cole Mayo <colemayo@utexas.edu>
2017-04-05 20:28:34 -0500

    there were indentaiton problems and lots of methods without closing parenthesis resolves #85

--- app/tests.py
+++ app/tests.py
@@ -12,7 +12,7 @@ from app.models import Beer, Brewery, Venue, State
 
 class TestModels(TestCase):
 
-	def setUp(self):
+    def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
@@ -53,81 +53,81 @@ class TestModels(TestCase):
 
     	
     def test_brewery(self):
-    	brewery = Brewery("0", "Hops and Grain", "micro", "2011", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "Diamond Food Mart, Westwood's Liquor" "Texas"
+    	brewery = Brewery("0", "Hops and Grain", "micro", "2011", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "Diamond Food Mart, Westwood's Liquor" "Texas")
     	db.session.add(brewery)
     	db.session.commit()
     	breweries = Brewery.query.all()
     	self.assertTrue(brewery in breweries)
     	
     def test_brewery2(self):
-    	brewery = Brewery("1", "Blue Owl", "medium", "2010", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "HEB" "Texas"
+    	brewery = Brewery("1", "Blue Owl", "medium", "2010", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "HEB" "Texas")
     	db.session.add(brewery)
     	db.session.commit()
     	breweries = Brewery.query.all()
     	self.assertTrue(brewery in breweries)
     	
     def test_brewery3(self):
-    	brewery = Brewery("2", "Zilker Brewing Company and Taproom", "micro", "2011", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "Diamond Food Mart, Westwood's Liquor" "Texas"
+    	brewery = Brewery("2", "Zilker Brewing Company and Taproom", "micro", "2011", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "Diamond Food Mart, Westwood's Liquor" "Texas")
     	db.session.add(brewery)
     	db.session.commit()
     	breweries = Brewery.query.all()
     	self.assertTrue(brewery in breweries)
     	
     def test_brewery4(self):
-    	brewery = Brewery("3", "Pinthouse Pizza", "large", "2002", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "Diamond Food Mart, Westwood's Liquor" "Texas"
+    	brewery = Brewery("3", "Pinthouse Pizza", "large", "2002", "https://www.tripadvisor.com/LocationPhotoDirectLink-g30196-d8547104-i232510009-Hops_Grain_Brewing-Austin_Texas.html", "507 Calles St, Suite 101, Austin, TX 78702", "Diamond Food Mart, Westwood's Liquor" "Texas")
     	db.session.add(brewery)
     	db.session.commit()
     	breweries = Brewery.query.all()
     	self.assertTrue(brewery in breweries)
 
     def test_venue(self):
-    	venue = Venue("0", "The Ginger Man", "https://www.facebook.com/pg/TheGingerManAustin/photos/", "301 Lavaca St.", "pub", "true", "Texas", "Spoetzl, Uncle Billy's", "Shiner Bock, Dos Equis, Budweiser"
+    	venue = Venue("0", "The Ginger Man", "https://www.facebook.com/pg/TheGingerManAustin/photos/", "301 Lavaca St.", "pub", "true", "Texas", "Spoetzl, Uncle Billy's", "Shiner Bock, Dos Equis, Budweiser")
     	db.session.add(venue)
     	db.session.commit()
     	venues = Venue.query.all()
     	self.assertTrue(venue in venues)
     	
     def test_venue2(self):
-    	venue = Venue("1", "Draught House Pub", "https://www.facebook.com/pg/Draughthouse/photos/", "879 Norther St.", "bar", "true", "Texas", "Spoetzl, Uncle Billy's", "Bud Light, Budweiser"
+    	venue = Venue("1", "Draught House Pub", "https://www.facebook.com/pg/Draughthouse/photos/", "879 Norther St.", "bar", "true", "Texas", "Spoetzl, Uncle Billy's", "Bud Light, Budweiser")
     	db.session.add(venue)
     	db.session.commit()
     	venues = Venue.query.all()
     	self.assertTrue(venue in venues)
     	
     def test_venue3(self):
-    	venue = Venue("2", "Craft Pride", "https://www.facebook.com/pg/craftpride/photos/", "3192 W Av", "pub", "true", "Florida", "Spoetzl, Uncle Billy's", "Heineken, Dos Equis, Budweiser"
+    	venue = Venue("2", "Craft Pride", "https://www.facebook.com/pg/craftpride/photos/", "3192 W Av", "pub", "true", "Florida", "Spoetzl, Uncle Billy's", "Heineken, Dos Equis, Budweiser")
     	db.session.add(venue)
     	db.session.commit()
     	venues = Venue.query.all()
     	self.assertTrue(venue in venues)
     	
     def test_venue4(self):
-    	venue = Venue("3", "Easy Tiger", "https://www.facebook.com/pg/easytiger/photos/", "888 N St.", "pub", "true", "Florida", "Spoetzl, Uncle Billy's", "Heineken, Dos Equis, Budweiser"
+    	venue = Venue("3", "Easy Tiger", "https://www.facebook.com/pg/easytiger/photos/", "888 N St.", "pub", "true", "Florida", "Spoetzl, Uncle Billy's", "Heineken, Dos Equis, Budweiser")
     	db.session.add(venue)
     	db.session.commit()
     	venues = Venue.query.all()
     	self.assertTrue(venue in venues)
 
     def test_state(self):
-    	state = State("0", "Austin", "Texas", "https://en.wikipedia.org/wiki/Flag_of_Texas#/media/File:Flag_of_Texas.svg", "TX", "Bluebonnet"
+    	state = State("0", "Austin", "Texas", "https://en.wikipedia.org/wiki/Flag_of_Texas#/media/File:Flag_of_Texas.svg", "TX", "Bluebonnet")
     	db.session.add(state)
     	db.session.commit()
     	states = State.query.all()
     	self.assertTrue(state in states)
     	
     def test_state2(self):
-    	state = State("1", "Tallahassee", "Florida", "https://en.wikipedia.org/wiki/Flag_of_Texas#/media/File:Flag_of_Florida.svg", "FL", "Orange Blossom"
+    	state = State("1", "Tallahassee", "Florida", "https://en.wikipedia.org/wiki/Flag_of_Texas#/media/File:Flag_of_Florida.svg", "FL", "Orange Blossom")
     	db.session.add(state)
     	db.session.commit()
     	states = State.query.all()
     	self.assertTrue(state in states)
     	
     def test_state3(self):
-    	state = State("2", "Atlanta", "Georgia", "https://en.wikipedia.org/wiki/Flag_of_Texas#/media/File:Flag_of_Georgia.svg", "GA", "Rosa laevigata"
+    	state = State("2", "Atlanta", "Georgia", "https://en.wikipedia.org/wiki/Flag_of_Texas#/media/File:Flag_of_Georgia.svg", "GA", "Rosa laevigata")
     	db.session.add(state)
     	db.session.commit()
     	states = State.query.all()
     	self.assertTrue(state in states)
     	
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
