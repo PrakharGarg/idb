75441392f75d65b2ef7a7008178fbc9bc3013c9f
Patrick Moore <phatrickmoore@gmail.com>
2017-04-05 22:02:14 -0500

    temp fixes cuz who knows!

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import sensitive
+#import sensitive
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = sensitive.DATABASE['postgres']
