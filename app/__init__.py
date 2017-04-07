eb85f8e8b463903bb248ac8f1351492ef2dcec1f
Cole Mayo <colemayo@utexas.edu>
2017-04-06 21:20:42 -0500

    uncommenting out the real database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-#import sensitive
+import sensitive
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = sensitive.DATABASE['postgres']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/poh'
+app.config['SQLALCHEMY_DATABASE_URI'] = sensitive.DATABASE['postgres']
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/poh'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.create_all()
