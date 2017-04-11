b9aee80a37e492d588854bed65648335acea8ff2
PrakharGarg <prakhargarg@utexas.edu>
2017-04-11 11:11:24 -0500

    react beers

--- project/static/scripts/jsx/cs.js
+++ project/static/scripts/jsx/cs.js
@@ -1,14 +1,59 @@
 require('whatwg-fetch'); 
 
-var foo = React.createClass({
-	render: function() {
-    
-    fetch('http://pursuitofhoppyness.me/api/beers')  
-    .then(function(response) {
-      
-    });
-	}
+var axios = require('axios');
+
+var App = React.createClass({
+
+  getInitialState: function() {
+    return {
+      beers: []
+    }
+  },
+
+  componentDidMount: function() {
+    var _this = this;
+    this.serverRequest = 
+      axios
+        .get("http://pursuitofhoppyness.me/api/beers")
+        .then(function(result) {    
+          _this.setState({
+            beers: result.data.result
+          });
+        })
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
+  render: function() {
+    return (
+      <div>
+          <h1>Beers!</h1>
+          {this.state.beers.map(function(beer) {
+            return (
+              <div className="col-md-3 col-sm-6 hero-feature text-center">
+							<div className="thumbnail">
+								<img src="{beer.label}" width = "150" alt=""/>
+								<div className="caption">
+									<h3>{beer.name}</h3>
+									<p>
+										{beer.style} <br/>
+										ABV: {beer.abv} <br />
+										Rating: {beer.rating}/5.0 
+									</p>
+									<p>
+										<a href="/beers/{{beer.id}}/" className="btn btn-primary">More Info</a>
+									</p>
+								</div>
+							</div>
+						</div>
+            );
+          })}
+        </div>
+    )
+  }
 });
 
-ReactDOM.render(React.createElement(foo,null),document.getElementById('app'));
+ReactDOM.render(<App/>,document.getElementById('app'));
 	
