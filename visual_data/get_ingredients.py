de27ac642d513193615c39bfbc8a9173ac0298ea
Drew Storey <adstorey@gmail.com>
2017-04-12 14:32:35 -0500

    completes first round of data collection for ingredient information

--- visual_data/get_ingredients.py
+++ visual_data/get_ingredients.py
@@ -16,17 +16,18 @@ def get_keys():
 def save_request():
 	keys = get_keys()
 	path = "http://api.vennfridge.appspot.com/ingredients/"
-	all_data = list()
-	"""for id in keys:
-		r = requests.get(path+str(id))
-		all_data+=[r.text]
-	"""
-	r = requests.get(path+str(keys[0]))
-	all_data+=[r.text]
-	data = {'result' : all_data}
-	#with open('test.txt','w') as fd:
-	#	json.dump(data, fd)
+	#subkeys = [keys[0], keys[1]]
+
 	with open('test.txt','w') as fd:
-		fd.write(data.text)
+		fd.write('{\n"result" : [ \n')
+
+	for id in keys:
+		r = requests.get(path+str(id))
+		with open('test.txt','a') as fd:
+			fd.write(r.text+',\n')
+
+	### DO NOT FORGET TO REMOVE LAST COMMA MANUALLY ###
+	with open('test.txt','a') as fd:
+		fd.write(']\n}')
 
 save_request()
