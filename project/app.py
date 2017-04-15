5c8c2678674010ae2abaee824fdbceb1b6d7d78d
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2017-04-14 19:04:03 -0500

    Example d3.js implemented

--- project/app.py
+++ project/app.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import unittest
 import io
+import numpy as np
+
 
 #import sensitive
 
@@ -45,6 +47,24 @@ def search(search_var) :
 @app.route('/visualization/')
 def visual() :
     return render_template('visualization.html')
+
+@app.route("/data")
+@app.route("/data/<int:ndata>")
+def data(ndata=100):
+    """
+    On request, this returns a list of ``ndata`` randomly made data points.
+    :param ndata: (optional)
+        The number of data points to return.
+    :returns data:
+        A JSON string of ``ndata`` data points.
+    """
+    x = 10 * np.random.rand(ndata) - 5
+    y = 0.5 * x + 0.5 * np.random.randn(ndata)
+    A = 10. ** np.random.rand(ndata)
+    c = np.random.rand(ndata)
+    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
+        "color": c[i]}
+        for i in range(ndata)])
 
 # Page that shows our technical report.
 @app.route('/technical/')
