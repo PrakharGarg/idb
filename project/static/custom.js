a1600cc2c629260184416a6d5c905737e85b41d0
PrakharGarg <prakhargarg@utexas.edu>
2017-04-16 15:02:34 -0500

    face lift

--- project/static/custom.js
+++ project/static/custom.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
+  
+  var $item = $('.carousel .item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
   // Get the modal
   var modal = document.getElementById('myModal');
   
