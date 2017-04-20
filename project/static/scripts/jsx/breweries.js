a8485494b7537eb9e4e8ca1daec33b3a5c6fa4a8
Amirali Hashim <amirali.hashim@gmail.com>
2017-04-20 15:07:02 -0500

    Filtering done fofor venues and breweries

--- project/static/scripts/jsx/breweries.js
+++ project/static/scripts/jsx/breweries.js
@@ -1,57 +1,215 @@
-require('whatwg-fetch'); 
-
 var axios = require('axios');
+var _ = require('lodash');
 
-var App = React.createClass({
-
-  getInitialState: function() {
-    return {
-      breweries: []
-    }
-  },
-
-  componentDidMount: function() {
-    var _this = this;
-    this.serverRequest = 
-      axios
-        .get("/api/breweries")
-        .then(function(result) {    
-          _this.setState({
-            breweries: result.data.result
-          });
-        })
-  },
-
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
-
-  render: function() {
+class Brewery extends React.Component {
+  render () {
+    var year = Math.floor(2017 - this.props.brewery.founded/365);
     return (
-      <div>
-          {this.state.breweries.map(function(brewery) {
-            return (
-              <div className="col-md-3 col-sm-6 hero-feature text-center">
-							<div className="thumbnail">
-								<img src={"" + brewery.label} width = "150" alt=""/>
-								<div className="caption">
-									<h3>{brewery.name}</h3>
-									<p>
-										{2017 - Math.floor(brewery.founded/365)} <br/>
-										{brewery.address}
-									</p>
-									<p>
-										<a href={"/breweries/" + brewery.id + "/"} className="btn btn-primary">More Info</a>
-									</p>
-								</div>
-							</div>
-						</div>
-            );
-          })}
+      <div className="col-md-3 col-sm-6 hero-feature text-center">
+        <div className="thumbnail">
+          <img src={"" + this.props.brewery.label} width = "150" alt=""/>
+          <div className="caption">
+            <h3>{this.props.brewery.name}</h3>
+            <p>
+              Brewery_Type: {this.props.brewery.brewery_type} <br />
+              Address: {this.props.brewery.address} <br />
+              Founded: {year} <br />
+            </p>
+            <p>
+              <a href={"/breweries/" + this.props.brewery.id + "/"} className="btn btn-primary">More Info</a>
+            </p>
+          </div>
         </div>
+      </div>
     )
   }
-});
+}
 
-ReactDOM.render(<App/>,document.getElementById('breweries'));
-	
+class ProductTable extends React.Component {
+  render() {
+    var rows = [];
+
+    var filteredProducts = _.filter(this.props.products, (brewery) => {
+      var type_matches = _.map(this.props.brewery_types, (brewery_type) => {
+        console.log(brewery_type + "  : " + brewery.brewery_type);
+        return _.contains(brewery.brewery_type.toLowerCase(), brewery_type);
+      });
+      var any_match = _.reduce(type_matches, (a,b) => {
+        return a || b;
+      }, false);
+      var type_matches = _.isEmpty(this.props.brewery_types) || any_match;
+      
+      return type_matches;
+    });
+
+    var sortedProducts = _.sortBy(filteredProducts, this.props.sortBy);
+    if (!this.props.ascend) {
+      sortedProducts.reverse();
+    }
+
+    sortedProducts.forEach((brewery) => {
+      rows.push(
+        <Brewery brewery={brewery}/>
+      );
+    });
+
+    return (
+      <div>{rows}</div>
+    );
+  }
+}
+
+class FilterBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSortByChange = this.handleSortByChange.bind(this);
+    this.handleOrderAscend = this.handleOrderAscend.bind(this);
+    this.handleOrderDescend = this.handleOrderDescend.bind(this);
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+
+  }
+
+  handleSortByChange(e) {
+    this.props.onSortChange(e.target.id);
+  }
+
+  handleOrderAscend(e) {
+    this.props.onOrderChange(true);
+  }
+
+  handleOrderDescend(e) {
+    this.props.onOrderChange(false);
+  }
+  handleTypeChange(e) {
+    this.props.onTypeChange(e.target.id);
+  }
+  
+
+  render() {
+    return (
+      <div className="col-md-3">
+        <div className="list-group">
+          <div className="dropdown">
+            <button className=" list-group-item btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Sort By
+              <span className="caret"></span></button>
+              <ul className="dropdown-menu" onClick={this.handleSortByChange}>
+                <li><a href="#" id="name">Name</a></li>
+                <li><a href="#" id='brewery_type'>Brewery Type</a></li>
+              </ul>
+          </div>
+
+          <button type="button" className="btn btn-default ascdesc" aria-label="Left Align" onClick={this.handleOrderAscend}>
+            <a href = "#" className="glyphicon glyphicon-arrow-up" aria-hidden="true"></a>
+          </button>
+
+          <button type="button" className="btn btn-default ascdesc" aria-label="Left Align" onClick={this.handleOrderDescend}>
+            <a href = "#" className="glyphicon glyphicon-arrow-down" aria-hidden="true"></a>
+          </button>
+          <div className="dropdown">
+            <button className=" list-group-item btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Brewery Type
+              <span className="caret"></span></button>
+              <ul className="dropdown-menu" onClick={this.handleTypeChange}>
+                <li className= "boxes" ><input id="micro brewery" type="checkbox"/> Micro Brewery <br /></li>
+                <li className= "boxes" ><input id="restaurant" type="checkbox"/> Restaurant <br /></li>
+                <li className= "boxes" ><input id="bar" type="checkbox"/> Bar <br /></li>
+              </ul>
+          </div>
+          
+
+          
+        </div>
+      </div>
+    )
+  }
+
+}
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breweries: new Array(),
+      sortBy: 'name',
+      ascend: true,
+      brewery_types: new Array()
+    };
+    
+    this.handleSortInput = this.handleSortInput.bind(this);
+    this.handleOrderInput = this.handleOrderInput.bind(this);
+    this.handleTypeInput = this.handleTypeInput.bind(this);
+
+  }
+
+  handleSortInput(sort_by) {
+    console.log(sort_by);
+    this.setState({
+      sortBy: sort_by
+    });
+  }
+
+  handleTypeInput(brewery_type) {
+    var newTypes = _.clone(this.state.brewery_types);
+    if (_.contains(this.state.brewery_types, brewery_type)) {
+      newTypes = _.filter(newTypes, (elem) => {
+        return elem !== brewery_type;
+      });
+    }
+    else {
+      newTypes.push(brewery_type);
+    }
+    this.setState({
+      brewery_types: newTypes
+    });
+    console.log(newTypes);
+  }
+
+  handleOrderInput(ascend) {
+    console.log(ascend);
+    this.setState({
+      ascend: ascend
+    });
+  }
+
+  
+
+  componentDidMount() {
+    var _this = this;
+    this.serverRequest = axios
+      .get("/api/breweries")
+      .then(function(result) {
+        console.log(result);   
+        _this.setState({
+          breweries: result.data.result
+        });
+      })
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
+  render() {
+    return (
+      <div className="grid row">
+        <FilterBar
+          onSortChange={this.handleSortInput}
+          onOrderChange={this.handleOrderInput}
+          onTypeChange={this.handleTypeInput}
+          
+        />
+        <ProductTable
+          products={this.state.breweries}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+
+          brewery_types={this.state.brewery_types}
+          sortBy={this.state.sortBy}
+          ascend={this.state.ascend}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<FilterableProductTable/>,document.getElementById('breweries'));
+  
--- project/static/scripts/jsx/venues.js
+++ project/static/scripts/jsx/venues.js
@@ -1,61 +1,214 @@
-require('whatwg-fetch'); 
-
 var axios = require('axios');
+var _ = require('lodash');
 
-var venuestyle = {
-  height: "50%"
-};
-
-var App = React.createClass({
-
-  getInitialState: function() {
-    return {
-      venues: []
-    }
-  },
-
-  componentDidMount: function() {
-    var _this = this;
-    this.serverRequest = 
-      axios
-        .get("/api/venues")
-        .then(function(result) {    
-          _this.setState({
-            venues: result.data.result
-          });
-        })
-  },
-
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
-
-  render: function() {
+class Venue extends React.Component {
+  render () {
     return (
-      <div>
-          {this.state.venues.map(function(venue) {
-            return (
-              <div className="col-md-3 col-sm-6 hero-feature text-center">
-							<div className="thumbnail">
-								<img className = "venueimg" style={venuestyle} src={"" + venue.media} width = "150" alt=""/>
-								<div className="caption">
-									<h3>{venue.name}</h3>
-									<p>
-										{venue.category} <br />
-                    {venue.address}
-									</p>
-									<p>
-										<a href={"/venues/" + venue.id + "/"} className="btn btn-primary">More Info</a>
-									</p>
-								</div>
-							</div>
-						</div>
-            );
-          })}
+      <div className="col-md-3 col-sm-6 hero-feature text-center">
+        <div className="thumbnail">
+          <img src={"" + this.props.venue.media} width = "150" alt=""/>
+          <div className="caption">
+            <h3>{this.props.venue.name}</h3>
+            <p>
+              Category: {this.props.venue.category} <br />
+              Address: {this.props.venue.address} <br />
+              Public: {this.props.venue.is_public} <br />
+            </p>
+            <p>
+              <a href={"/venues/" + this.props.venue.id + "/"} className="btn btn-primary">More Info</a>
+            </p>
+          </div>
         </div>
+      </div>
     )
   }
-});
+}
 
-ReactDOM.render(<App/>,document.getElementById('venues'));
-	
+class ProductTable extends React.Component {
+  render() {
+    var rows = [];
+
+    var filteredProducts = _.filter(this.props.products, (venue) => {
+      var type_matches = _.map(this.props.categories, (category) => {
+        console.log(category + "  : " + venue.category);
+        return _.contains(venue.category.toLowerCase(), category);
+      });
+      var any_match = _.reduce(type_matches, (a,b) => {
+        return a || b;
+      }, false);
+      var type_matches = _.isEmpty(this.props.categories) || any_match;
+      
+      return type_matches;
+    });
+
+    var sortedProducts = _.sortBy(filteredProducts, this.props.sortBy);
+    if (!this.props.ascend) {
+      sortedProducts.reverse();
+    }
+
+    sortedProducts.forEach((venue) => {
+      rows.push(
+        <Venue venue={venue}/>
+      );
+    });
+
+    return (
+      <div>{rows}</div>
+    );
+  }
+}
+
+class FilterBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSortByChange = this.handleSortByChange.bind(this);
+    this.handleOrderAscend = this.handleOrderAscend.bind(this);
+    this.handleOrderDescend = this.handleOrderDescend.bind(this);
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+
+  }
+
+  handleSortByChange(e) {
+    this.props.onSortChange(e.target.id);
+  }
+
+  handleOrderAscend(e) {
+    this.props.onOrderChange(true);
+  }
+
+  handleOrderDescend(e) {
+    this.props.onOrderChange(false);
+  }
+  handleTypeChange(e) {
+    this.props.onTypeChange(e.target.id);
+  }
+  
+
+  render() {
+    return (
+      <div className="col-md-3">
+        <div className="list-group">
+          <div className="dropdown">
+            <button className=" list-group-item btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Sort By
+              <span className="caret"></span></button>
+              <ul className="dropdown-menu" onClick={this.handleSortByChange}>
+                <li><a href="#" id="name">Name</a></li>
+                <li><a href="#" id='category'>Category</a></li>
+              </ul>
+          </div>
+
+          <button type="button" className="btn btn-default ascdesc" aria-label="Left Align" onClick={this.handleOrderAscend}>
+            <a href = "#" className="glyphicon glyphicon-arrow-up" aria-hidden="true"></a>
+          </button>
+
+          <button type="button" className="btn btn-default ascdesc" aria-label="Left Align" onClick={this.handleOrderDescend}>
+            <a href = "#" className="glyphicon glyphicon-arrow-down" aria-hidden="true"></a>
+          </button>
+          <div className="dropdown">
+            <button className=" list-group-item btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Category
+              <span className="caret"></span></button>
+              <ul className="dropdown-menu" onClick={this.handleTypeChange}>
+                <li className= "boxes" ><input id="bar" type="checkbox"/> Bar <br /></li>
+                <li className= "boxes" ><input id="restaurant" type="checkbox"/> Restaurant <br /></li>
+                <li className= "boxes" ><input id="brewery" type="checkbox"/> Brewery <br /></li>
+              </ul>
+          </div>
+          
+
+          
+        </div>
+      </div>
+    )
+  }
+
+}
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      venues: new Array(),
+      sortBy: 'name',
+      ascend: true,
+      categories: new Array()
+    };
+    
+    this.handleSortInput = this.handleSortInput.bind(this);
+    this.handleOrderInput = this.handleOrderInput.bind(this);
+    this.handleTypeInput = this.handleTypeInput.bind(this);
+
+  }
+
+  handleSortInput(sort_by) {
+    console.log(sort_by);
+    this.setState({
+      sortBy: sort_by
+    });
+  }
+
+  handleTypeInput(category) {
+    var newTypes = _.clone(this.state.categories);
+    if (_.contains(this.state.categories, category)) {
+      newTypes = _.filter(newTypes, (elem) => {
+        return elem !== category;
+      });
+    }
+    else {
+      newTypes.push(category);
+    }
+    this.setState({
+      categories: newTypes
+    });
+    console.log(newTypes);
+  }
+
+  handleOrderInput(ascend) {
+    console.log(ascend);
+    this.setState({
+      ascend: ascend
+    });
+  }
+
+  
+
+  componentDidMount() {
+    var _this = this;
+    this.serverRequest = axios
+      .get("/api/venues")
+      .then(function(result) {
+        console.log(result);   
+        _this.setState({
+          venues: result.data.result
+        });
+      })
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
+  render() {
+    return (
+      <div className="grid row">
+        <FilterBar
+          onSortChange={this.handleSortInput}
+          onOrderChange={this.handleOrderInput}
+          onTypeChange={this.handleTypeInput}
+          
+        />
+        <ProductTable
+          products={this.state.venues}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+
+          categories={this.state.categories}
+          sortBy={this.state.sortBy}
+          ascend={this.state.ascend}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<FilterableProductTable/>,document.getElementById('venues'));
+  
